fedb060589ee867217bea72a9fdc8d0feab6ec48
ksrilaya <71928519+ksrilaya@users.noreply.github.com>
2023-09-30 12:05:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 //your code here!
-let scrollcontainer=document.getElementById("infi-list");
-let i=0;
-function addoneItem(){
-	const li = document.createElement("li");
-	li.innertext=`Item ${++i}`;
-	scrollcontainer.appendChild(li);
+const scrollContainer = document.getElementById("infi-list");
+let i = 0;
+function addOneItem() {
+  const li = document.createElement("li");
+  li.innerText = `Item ${++i}`;
+  scrollContainer.appendChild(li);
 }
-for (let i=0;i<10;i++){
-	addoneItem();
-}
-scrollcontainer.addEventListener("scroll",()=>{
-	const maxscrollHeight = scrollcontainer.scrollHeight-scrollcontainer.clientHeight;
-	let currentScrollTop=scrollcontainer.scrollTop;
-	let threshold=Math.abs(maxscrollHeight-currentScrollTop);
 
-	if(threshold<=5){
-		for(let i=0;i<2;i++){
-			addoneItem();
-		}
-	}
+for (let i = 0; i < 10; i++) {
+  addOneItem();
+}
+
+scrollContainer.addEventListener("scroll", () => {
+  const maxScrollHeight =
+    scrollContainer.scrollHeight - scrollContainer.clientHeight;
+  let currentScrollTop = scrollContainer.scrollTop;
+
+  let threshold = Math.abs(maxScrollHeight - currentScrollTop);
+
+  if (threshold <= 5) {
+    for (let i = 0; i < 2; i++) {
+      addOneItem();
+    }
+  }
 });
-
