96e800a741b7e2d6038359b5fe9f92db830254f1
ksrilaya <71928519+ksrilaya@users.noreply.github.com>
2023-09-30 11:59:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
+let scrollcontainer=document.getElementById("infi-list");
+let i=0;
+function addoneItem(){
+	const li = document.createElement("li");
+	li.innertext=`Item ${++i}`;
+	scrollcontainer.appendchild(li);
+}
+for (let i=0;i<10;i++){
+	addoneItem();
+}
+scrollcontainer.addEventListener("scroll",()=>{
+	const maxscrollHeight = scrollcontainer.scrollHeight-scrollcontainer.clientHeight;
+	let currentScrollTop=scrollcontainer.scrollTop;
+	let threshold=Math.abs(maxscrollHeight-currentScrollTop);
+
+	if(threshold<=5){
+		for(let i=0;i<2;i++){
+			addoneItem();
+		}
+	}
+});
 
